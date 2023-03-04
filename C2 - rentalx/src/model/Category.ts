7f46a5b088751933dd04945ef9f503e907693cb5
Gustavo Moura Barros <gustavomourago@gmail.com>
2023-03-03 23:46:09 -0300

    C2 COMPLETED

---
+++ C2 - rentalx/src/model/Category.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 as uuidV4 } from 'uuid';
+
+class Category {
+    id?: string;
+    name: string;
+    description: string;
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+        }
+    }
+}
+
+export { Category };
