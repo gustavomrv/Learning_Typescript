8681ea0471c5fa2d073cd7f1e853eab6aa5d238f
Gustavo Moura Barros <gustavomourago@gmail.com>
2023-03-03 22:09:34 -0300

    Ambiente Configurado

---
+++ C2 - rentalx/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'hi' });
+});
+
+app.listen(3333, () => console.log('Server running'));
